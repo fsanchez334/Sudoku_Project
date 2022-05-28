377f1fdff0fdce6a6c660642431ed1b1d1430a68
Fernando Sanchez <fernandosanchez955@gmail.com>
2022-05-28 15:43:56 -0400

    Added a main function

--- sudokuproject.py
+++ sudokuproject.py
@@ -149,9 +149,12 @@ def solve(boarder):
     boarder.make_move(coordinate, 0)
 
 """## Step 4: Defining the Main Function"""
+def main():
+  starter = "000260701680070090190004500820100040004602900050003028009300074040050036703018000"
+  start = [int(x) for x in starter]
+  boarder = Sudoku(start)
+  answer = solve(boarder)
+  answer
 
-starter = "000260701680070090190004500820100040004602900050003028009300074040050036703018000"
-start = [int(x) for x in starter]
-boarder = Sudoku(start)
-answer = solve(boarder)
-answer
+if __name__ == '__main__':
+  main()
